fe0bb5203945d5f9d2c2d879a33be0849e91aee9
Adam Parker <adam@winfree.me.uk>
2013-06-11 10:40:31 +0100

    Complete QA process

--- multimediadb/admin.py
+++ multimediadb/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from multimediadb.models import Aircrafttype, Aircraftsystem, Systemgraphic, Graphicworkdone
+from multimediadb.models import Aircrafttype, Aircraftsystem, Systemgraphic, Graphicworkdone, Comments, Uploads, QA
 
 class TypesAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
@@ -13,7 +13,20 @@ class GraphicAdmin(admin.ModelAdmin):
 class GraphicWDAdmin(admin.ModelAdmin):
     list_display = ('systemgraphic','work_carried_out',)
 
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('source','source_id','comment',)
+    
+class UploadsAdmin(admin.ModelAdmin):
+    list_display = ('file','description',)
+
+class QAAdmin(admin.ModelAdmin):
+    list_display = ('systemgraphic','qa_stage','result',)
+
+
 admin.site.register(Aircrafttype, TypesAdmin)
 admin.site.register(Aircraftsystem, SystemAdmin)
 admin.site.register(Systemgraphic, GraphicAdmin)
 admin.site.register(Graphicworkdone, GraphicWDAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Uploads, UploadsAdmin)
+admin.site.register(QA, QAAdmin)
